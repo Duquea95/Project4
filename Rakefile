4400169aa03a023d3357b6fb1f3439fef77786e8
Anthony Duque <duquea95@gmail.com>
2016-09-23 12:26:22 -0400

    Based off of happy_seed: base included

---
+++ Rakefile
@@ -0,0 +1,12 @@
+# Add your own tasks in files placed in lib/tasks ending in .rake,
+# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
+
+require_relative 'config/application'
+
+module TempFixForRakeLastComment
+  def last_comment
+    last_description
+  end
+end
+Rake::Application.send :include, TempFixForRakeLastComment
+Rails.application.load_tasks
---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module BikeMe
+  class Application < Rails::Application
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
